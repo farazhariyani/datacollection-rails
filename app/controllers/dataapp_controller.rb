ba8bc573e61cee643673a549e9d4c4de03debc4d
farazhariyani <farazhariyani7@gmail.com>
2020-03-18 12:16:02 +0530

    data collection web-app

---
+++ app/controllers/dataapp_controller.rb
@@ -0,0 +1,7 @@
+class DataappController < ApplicationController
+
+    def index
+
+    end
+
+end
---
+++ test/controllers/dataapp_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DataappControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
